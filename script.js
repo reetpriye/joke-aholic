225d76f688e1e085e499ae59ce643a65cd957f26
Reet Priye <reetpriye@gmail.com>
2020-11-09 16:41:44 +0530

    Feature Improvement: add loading svg

--- script.js
+++ script.js
@@ -5,9 +5,25 @@ const joke2Text = document.getElementById("joke2");
 const jokeType = document.getElementById("joke-type");
 const wpBtn = document.getElementById("whatsapp");
 const newJokeBtn = document.getElementById("new");
+const loader = document.getElementById("loader");
+const jokeArrow1 = document.getElementById("joke-arrow-right-1");
+const jokeArrow2 = document.getElementById("joke-arrow-right-2");
+
+function showLoader() {
+    loader.hidden = false;
+    jokeContainer.hidden = true;
+}
+
+function hideLoader() {
+    if(!loader.hidden) {
+        loader.hidden = true;
+        jokeContainer.hidden = false;
+    }
+}
 
 // Get Joke from API
 async function getJoke() {
+    showLoader();
     const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Any?type=twopart';
     try {
         const response = await fetch(apiUrl);
@@ -16,10 +32,14 @@ async function getJoke() {
         if(data.setup.length+data.delivery.length>80) {
             joke1Text.classList.add('long-joke');
             joke2Text.classList.add('long-joke');
+            jokeArrow1.classList.add('long-joke-arrow-right');
+            jokeArrow2.classList.add('long-joke-arrow-right');
         }
         else {
             joke1Text.classList.remove('long-joke');
             joke2Text.classList.remove('long-joke');
+            jokeArrow1.classList.remove('long-joke-arrow-right');
+            jokeArrow2.classList.remove('long-joke-arrow-right');
         }
         joke1Text.innerText = `Abby : ${data.setup}`;
         joke2Text.innerText = `Gabby : ${data.delivery}`;
@@ -27,6 +47,7 @@ async function getJoke() {
     } catch (error) {
        console.log(`Error Reported: ${error}`); 
     }
+    hideLoader();
 }
 
 function shareOnWp() {
